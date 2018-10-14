a0aa8c043b882ee7c557d00af65bb0f6679c8449
cacila <37920077+cacila@users.noreply.github.com>
2018-10-14 20:22:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = process.env.MONGODB_URI||'mongodb://kacila:954ylg5510@ds125723.mlab.com:25723/local_library';
+var mongoDB = process.env.MONGODB_URI；
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
